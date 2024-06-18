79510967380165abbe92923cc6205feb5802d82c
johancamilo <harolmoreno1000@gmail.com>
2024-06-18 17:53:46 -0500

    23132

--- app/components/modal.tsx
+++ app/components/modal.tsx
@@ -5,7 +5,7 @@ interface ModalProps {
   onClose: () => void; // Función que no devuelve nada
   children: React.ReactNode; // Tipo genérico para cualquier cosa que pueda ser renderizada en React
 }
-import { IoIosCloseCircle } from "react-icons/io";
+import { IoCloseCircle } from "react-icons/io5";
 
 const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
@@ -17,33 +17,33 @@ const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-        <div className={`inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle  ${isOpen? 'block' : 'hidden'}`}>
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 flex flex-col items-center justify-center space-y-16">
+        <div className={`inline-block align-bottom bg-white text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle w-[35%] px-4 ${isOpen? 'block' : 'hidden'}`}>
+          <div className="bg-white  flex flex-col items-center justify-center ">
             {children}
-            <div className="flex items-center justify-center text-4xl border border-gray-400 rounded-full h-16 w-16">
-                <span className="">x</span>
+            <div className="flex justify-end text-4xl w-full mt-2">
+                <IoCloseCircle className="text-gray-400 hover:text-gray-500" onClick={onClose}/>
             </div>
-            <h3 className="text-4xl leading-6 font-medium text-blue-500 mt-12">50% Complete</h3>
-            <div className="flex w-full">
-              <div className="bg-red-500 w-full h-2"></div>
-              <div className="bg-blue-500 w-full h-2"></div>
+            <h3 className="text-1xl leading-6 font-medium text-blue-400 mb-2">50% Complete</h3>
+            <div className="flex w-[70%] mb-12 rounded-full bg-gray-500">
+              <div className="bg-blue-400 w-full h-2 flex-grow rounded-full"></div>
+              <div className="bg-gray-500 w-full h-2 flex-grow rounded-full"></div>
             </div>
-            <div className="">
-              <p className="text-4xl text-gray-500 text-center">ALMOST THERE COMPLETE THE FORM BELOW TO GET INSTANT ACCESS.</p>
+            <div className="mb-12 px-10">
+              <p className="text-md text-gray-500 text-center">ALMOST THERE COMPLETE THE FORM BELOW TO GET INSTANT ACCESS.</p>
             </div>
-            <div className="">
-              <p className="text-5xl text-gray-500 text-center">Enter Your Email Address to Access the First Lesson of the Course:</p>
+            <div className="px-6">
+              <p className="text-2xl text-gray-500 text-center">Enter Your Email Address to Access the First Lesson of the Course:</p>
             </div>
           </div>
-          <div className="px-8">
-              <p className="text-3xl">Email</p>
-              <input type="text" className="border border-red-400" placeholder="Email addres" />
+          <div className="px-12">
+              <p className="text-1xl">Email</p>
+              <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-sm focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Email addres" />  
           </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 ">
-            <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" onClick={onClose}>
-              Cerrar
-            </button>
-          </div>
+          <div className="py-4 px-12 flex items-center justify-center ">
+              <button type="button" className="w-full inline-flex justify-center border border-transparent shadow-sm px-4 py-6 bg-red-600 text-2xl font-bold text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500" >
+                Sing Up
+              </button>
+            </div>
         </div>
       </div>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -20,45 +20,45 @@ export default function Home() {
   };
 
   return (
-    <div className="mx-[20%] px-4 sm:px-6 lg:px-8 md:w-[65%] mt-36">
-    <div className="mb-64 ">
-      <div className="py-12 px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 mb-52">
-        <img src="header.png"/>
+    <div className="mx-2 md:mx-[18%]  md:px-8 md:w-[65%]">
+      <div className="md:px-12  mt-12 md:mt-24">
+        <img src="header.png" width={300} height={300}/>
       </div>
-      <div className="px-6 sm:px-8 md:px-10 lg:px-12 xl:px-14 ">
-        <h2 className="text-5xl font-bold leading-tight sm:text-8xl">
-          10 essential makeup<span className="block mb-2 md:mb-6"></span>
-          techniques to create your<span className="block mb-2 md:mb-6"></span>
+      <div className=" sm:px-8 md:px-10 lg:px-12 xl:px-14 mt-12 md:mt-32">
+        <h2 className="text-5xl font-bold leading-tight sm:text-5xl">
+          10 essential makeup<span className="block mb-2 md:mb-4"></span>
+          techniques to create your<span className="block mb-2 md:mb-4"></span>
           perfect look.
         </h2>
       </div>
-    </div>
-    <div className="flex">
-      <div className="bg-pink-100  space-y-16 px-6 md:px-20 py-12 w-[50%]">
-        <span className="text-green font-bold text-2xl">
+    <div className=" flex flex-col mt-32 md:ml-4 md:flex-row ">
+      <div className="bg-red-100  space-y-4 md:space-y-18 md:px-10 py-2 md:py-6 md:w-[55%] px-4">
+        <span className="text-green font-bold text-lg ">
           STEP 1
         </span>
-        <h2 className="font-bold sm:text-7xl">
+        <h2 className="font-bold sm:text-3xl">
           WATCH THE VIDEO
         </h2>
     
         <div className="w-full h-auto">
-          <img src="/video.png" className=" inset-0 w-full h-full object-cover"/>
+          <video controls className="inset-0 w-full h-full object-cover">
+            <source src="/video.mp4" type="video/mp4" />
+          </video>
         </div>
       </div>
-      <div className="bg-white space-y-16 px-6 md:px-20 py-12 w-[40%] ">
-          <span className="text-green font-bold text-2xl">
+      <div className="bg-white space-y-4 md:space-y-18 md:px-10 py-2 md:py-6 md:w-[45%] px-4">
+          <span className="text-green font-bold text-lg ml-6">
             STEP 2 
           </span>
-          <h2 className="font-bold sm:text-7xl">
+          <h2 className="font-bold sm:text-3xl ml-6">
               TAKE THE COURSE
             </h2> 
-          <div className=" flex flex-col justify-between items-center h-[30vh]">
+          <div className=" flex flex-col justify-between items-center h-[35vh]">
                   <div className="h-64"></div>
-                  <div className="h-full">
-                    <img src="/boton.png"/>
+                  <div className="h-full ">
+                    <img src="/boton.png" width={140} height={140}/>
                   </div>
-                <button onClick={openModal} className="w-full h-64 bg-red-700 hover:bg-red-800 text-white text-4xl px-32 font-bold">SUSCRIBIRSE OF COURSE</button>
+                <button onClick={openModal} className="w-[80%] bg-red-700 hover:bg-red-800 text-white text-xl p-6 font-bold mt-6">SUSCRIBIRSE OF COURSE</button>
                   <Modal isOpen={isModalOpen} onClose={closeModal}>
                     
                 </Modal>
@@ -66,7 +66,7 @@ export default function Home() {
 
         </div>
     </div>
-    <footer className="flex items-center justify-center text-3xl py-32 w-full ">
+    <footer className="flex items-center justify-center text-1xl w-full mt-24 mb-16">
       <div>
       ©2023 Mirage Beauty Coach. All rights reserved.  Privacy Policy  |  Created with Leadpages
       </div>
